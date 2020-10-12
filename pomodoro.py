08fed91b196730f2caa2f46fcdf72400f5da89ef
eg-vw <61621232+eg-vw@users.noreply.github.com>
2020-10-12 15:10:06 +0200

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -5,18 +5,18 @@ def pomodoro():
         count = 0
         if 0 != 1:
                 while True:
-                        os.system( "say time to work" )
+                        os.system("say time to work")
                         print('Pomodoro work time!')
                         time.sleep(1500) 
                         count +=1
                         
                         if count % 3:
-                                os.system( "say 5 minute break" )
+                                os.system("say 5 minute break")
                                 print('Pomodoro 5 minute break!')
                                 time.sleep(300) 
                                 
                         else:
-                                os.system( "say 15 minute break" )
+                                os.system("say 15 minute break")
                                 print('Pomodoro chill time!')
                                 time.sleep(900)                         
                 
