583276961f53463741af0cc6880ac3f15eeed2e3
eg-vw <61621232+eg-vw@users.noreply.github.com>
2020-10-12 12:07:44 +0200

    Add files via upload

---
+++ pomodoro.py
@@ -0,0 +1,25 @@
+import time
+import os
+
+def pomodoro():
+        count = 0
+        if 0 != 1:
+                while True:
+                        os.system( "say time to work" )
+                        print('Pomodoro work time!')
+                        time.sleep(1500) 
+                        count +=1
+                        print(count)
+                        
+                        if count % 3:
+                                os.system( "say 5 minute break")
+                                print('Pomodoro 5 minute break!')
+                                time.sleep(300) 
+                                
+                        else:
+                                os.system( "say 15 minute break" )
+                                print('Pomodoro chill time!')
+                                time.sleep(900)                         
+                
+            
+pomodoro()
